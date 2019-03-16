cbcb3202b1c9dcfe931ae3820e84c7f8f7214c79
FIRST_NAME LAST_NAME <47652529+Kevinufo@users.noreply.github.com>
2019-03-16 13:46:41 -0700

    Initial Changes for pixel editor

---
+++ index.js
@@ -0,0 +1,20 @@
+function amsterdam(pixel){
+  pixel.style.backgroundColor = bob;
+}
+
+var bob = "orange";
+function setcolor(color){
+bob = color;
+}
+
+function randompixelcolor(){
+bob = randomColor()
+}
+
+
+
+
+
+
+
+
